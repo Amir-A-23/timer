5e8e97020522596965e742cb94581e6cf3678972
Amir <amir.a.23@hotmail.com>
2021-11-03 04:17:16 +0000

    updated to lint code

--- timer1.js
+++ timer1.js
@@ -1,15 +1,16 @@
 const args = process.argv.slice(2);
 const times = args.map(Number);
+let delay = 0;
 times.forEach((nums) => {
   delay = nums * 1000;
-  if(!nums) return;
-  else if(nums < 0) return;
-  else if(typeof (nums) !== 'number') return;
+  if (!nums) return;
+  else if (nums < 0) return;
+  else if (typeof (nums) !== 'number') return;
   else {
     setTimeout(() => {
       process.stdout.write('beep\n');
       process.stdout.write('\x07');
-    }, delay)
-   // console.log(delay)
+    }, delay);
+    // console.log(delay)
   }
 });
