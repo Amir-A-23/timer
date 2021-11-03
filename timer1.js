395002a480f870e652706e525490a8eb3941874d
Amir <amir.a.23@hotmail.com>
2021-11-03 04:14:42 +0000

    created timer scheduled to async setTimeout and outputting beeps

---
+++ timer1.js
@@ -0,0 +1,15 @@
+const args = process.argv.slice(2);
+const times = args.map(Number);
+times.forEach((nums) => {
+  delay = nums * 1000;
+  if(!nums) return;
+  else if(nums < 0) return;
+  else if(typeof (nums) !== 'number') return;
+  else {
+    setTimeout(() => {
+      process.stdout.write('beep\n');
+      process.stdout.write('\x07');
+    }, delay)
+   // console.log(delay)
+  }
+});
